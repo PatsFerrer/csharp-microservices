c0f7605deea4c5ecb3cb821d02303e98f84de818
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-10-11 10:31:55 -0300

    espaço

--- src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -2,7 +2,5 @@
 
 namespace BuildingBlocks.CQRS
 {
-    public interface IQuery<out TResponse> : IRequest<TResponse> where TResponse : notnull
-    {
-    }
+    public interface IQuery<out TResponse> : IRequest<TResponse> where TResponse : notnull { }
 }
