543328d079c502cd0abb03a6e7ffe8ce29597ed2
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-10-11 11:31:29 -0300

    cria uma entidade de produto a partir do obj de command

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,17 +1,29 @@
 ﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
-    public record CreateProductResult(Guid id); // representa o obj de retorno, nesse caso retorna o ID
+    public record CreateProductResult(Guid id); // representa o obj de retorno ^ aqui de cima, nesse caso retorna o ID
     internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            // Business logic to create a product
+            // criar uma entidade de produto a partir do obj de command
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
 
-            throw new NotImplementedException();
+            // TODO: salvar no banco de dados
+
+            // retorna o resultado de criação de produto
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
